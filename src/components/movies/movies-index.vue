6fd681dc06c15dd24fb2dd7b082b1e6d2248019a
Mike Fritzsche <mike@mikefritzsche.com>
2022-08-16 10:28:25 -0700

    add movies index component, update circleci config slack custom template

---
+++ src/components/movies/movies-index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <span>{{ category }}</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps} from 'vue'
+// import type { Ref } from 'vue'
+
+interface Props {
+  category: string
+}
+
+const props = defineProps<Props>()
+console.log('props: ', props.category)
+
+
+
+
+</script>
---
+++ src/views/MoviesView.vue
@@ -0,0 +1,10 @@
+<template>
+  <Movies :category="category"/>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import Movies from '@/components/movies/movies-index.vue'
+const category = ref('now_playing')
+
+</script>
